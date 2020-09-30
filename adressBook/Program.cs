e8767fbbc208a21a93e31ecbf5ba6502c0b9197d
venkey <venkeyvenkatesh885@gmail.com>
2020-09-30 14:41:44 +0530

    [venkey] Refactor. Added the second contact

--- adressBook/Program.cs
+++ adressBook/Program.cs
@@ -10,11 +10,15 @@ namespace adressBook
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book program");
 
+            Console.WriteLine("Welcome to Address Book program");
+           
             contactBook firstContact = new contactBook("venkey", "venkatesh", "rjy", "AP", 533293, "9866256666", "venkey@abc.com");
 
+            contactBook secondContact = new contactBook("Rajesh", "naidu", "vskp", "TS", 580466, "9492227597", "abc@gmail.com");
+
             firstContact.displayContact();
+            secondContact.displayContact();
         }
     }
 }
